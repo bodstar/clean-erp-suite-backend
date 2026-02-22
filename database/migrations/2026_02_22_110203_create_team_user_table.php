acd92f07f9b01403b5872f0abd02d4528084ece0
Emmanuel Nunoo <e.nunoo36@gmail.com>
2026-02-22 12:19:28 +0000

    created pivot table for user-team relationship

---
+++ database/migrations/2026_02_22_110203_create_team_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('team_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->unique(['team_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('team_user');
+    }
+};
