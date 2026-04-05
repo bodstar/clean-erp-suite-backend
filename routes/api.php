e187ad1b618f0badc09c6eb06ce2a03c07694f01
Emmanuel Nunoo <e.nunoo36@gmail.com>
2026-04-05 11:07:59 +0000

    added mpromo api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\MPromo\PartnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
@@ -19,6 +20,19 @@ Route::prefix('auth')->group(function () {
 Route::middleware(['auth:sanctum', ResolveTeamFromHeader::class])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum', ResolveTeamFromHeader::class])
+    ->prefix('mpromo')
+    ->group(function () {
+        Route::get('partners', [PartnerController::class, 'index']);
+        Route::post('partners', [PartnerController::class, 'store']);
+        Route::get('partners/{partner}', [PartnerController::class, 'show']);
+        Route::put('partners/{partner}', [PartnerController::class, 'update']);
+
+        Route::put('partners/{partner}/geolocation', [PartnerController::class, 'updateGeolocation']);
+        Route::post('partners/{partner}/suspend', [PartnerController::class, 'suspend']);
+        Route::post('partners/{partner}/activate', [PartnerController::class, 'activate']);
+    });
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
